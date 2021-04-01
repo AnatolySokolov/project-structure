ae5b10a5ce1296add6e86b142ec59f0b3cc912bf
anatoly <anatoliysokolov1981@gmail.com>
2021-04-01 23:12:31 +0300

    fix: dashboard column chart formatheading function has been added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -70,11 +70,24 @@ export default class Page {
       link:'#'
     });
 
+    function getFormatHeading(number, divider) {
+      const arr = String(number).split('');      
+
+      if (arr.length > 3 && arr.length < 6) {
+        arr.splice(-3, 0, divider);
+      } else if (arr.length > 6) {
+        arr.splice(-3, 0, divider);
+        arr.splice(-7, 0, divider);
+      }
+
+      return `$${arr.join('')}`;
+    }
+
     const salesChart = new ColumnChart({
       url: 'api/dashboard/sales',
       range: dateRange,
       label: 'sales',
-      formatHeading: data => `$${data}`
+      formatHeading: data => getFormatHeading(data, ',')
     });
 
     const customersChart = new ColumnChart({
