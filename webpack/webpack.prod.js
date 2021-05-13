1ab77d45721594581140fa9ad6986eeff082e165
anatoly <anatoliysokolov1981@gmail.com>
2021-05-13 23:16:33 +0300

    fix: public path refactoring

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   output: {
-    publicPath: '/',
+    publicPath: 'https://anatolysokolov.github.io/project-structure/',
     path: path.resolve(__dirname, '../build')
   },
   optimization: {
